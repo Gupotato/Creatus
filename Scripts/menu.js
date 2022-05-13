fd0581783516d5549794412bae7ddc8638ef635f
Bruno Hideyuki Takeuchi <bhtakeuchi45@gmail.com>
2022-05-13 18:04:01 -0300

    menu implemented

---
+++ Scripts/menu.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "25vw";
+    document.getElementById("main").style.marginLeft = "25vw";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
