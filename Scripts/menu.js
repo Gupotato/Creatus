d788f02dc346a66665ca100f5b8113498afa18fe
Bruno Hideyuki Takeuchi <bhtakeuchi45@gmail.com>
2022-05-20 17:02:14 -0300

    carrousel implemented

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -8,3 +8,22 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+        
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+  }
